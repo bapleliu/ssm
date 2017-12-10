40d883db0becb47564bbb76b22ca4fabae42d5fb
lbp <qdliubp@126.com>
2017-12-10 14:32:46 +0800

    优化

--- src/main/java/com/ssm/model/User.java
+++ src/main/java/com/ssm/model/User.java
@@ -18,6 +18,38 @@ public class User {
 
     private Integer age;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
     @Override
     public String toString() {
         return "User{" +
